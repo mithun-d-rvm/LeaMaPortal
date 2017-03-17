af00d342e8431f8d07d805121770bcd517fa787e
Somasundharam <somuit91@gmail.com>
2017-03-17 20:39:01 +0530

    Removed the unwanted changes

--- LeaMaPortal/Models/CountryViewModel.cs
+++ LeaMaPortal/Models/CountryViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace LeaMaPortal.Controllers
+namespace LeaMaPortal.Models
 {
     public class CountryViewModel
     {
