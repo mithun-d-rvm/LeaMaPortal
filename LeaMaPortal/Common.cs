d91f1281907f64b9ae42e5b968f74448fc60955b
Somasundharam <somuit91@gmail.com>
2017-04-05 00:15:48 +0530

    tca changes

--- LeaMaPortal/Common.cs
+++ LeaMaPortal/Common.cs
@@ -7,17 +7,19 @@ namespace LeaMaPortal
 {
     public static class Common
     {
-        public static List<string> Title = new List<string>() {"MR.", "MRS.","MS." };
-        public const string DefaultTitle= "MR.";
-        //public static List<string> City = new List<string>() { "Abudhabi", "Sharja" };
-        public static List<string> Profession = new List<string>() { "Engineer", "Teacher", "Shop keeper","Doctor","Farmer" };
+        public static List<string> Title = new List<string>() { "MR.", "MRS.", "MS." };
+        public static List<string> TenentType = new List<string>() { "Company", "Individual" };
+        public const string DefaultTitle = "MR.";
 
-        public const string INSERT = "INSERT", UPDATE = "UPDATE", DELETE = "DELETE", SELECT= "SELECT",View = "View";
+        //public static List<string> City = new List<string>() { "Abudhabi", "Sharja" };
+        public static List<string> Profession = new List<string>() { "Engineer", "Teacher", "Shop keeper", "Doctor", "Farmer" };
+
+        public const string INSERT = "INSERT", UPDATE = "UPDATE", DELETE = "DELETE", SELECT = "SELECT", View = "View";
         public const int DefaultMaster = 10;
         public const string TenantIndividualDocumentContainer = "Documents/TenantIndividual/";
 
         //tenant company
-        public static List<string> TenantType = new List<string>() { "Government","Person", "Company" };
+        public static List<string> TenantType = new List<string>() { "Government", "Person", "Company" };
         public static List<string> Emirate = new List<string>() { "Default" };
         public static List<string> ComapanyActivity = new List<string>() { "Activity1" };
         public static List<string> Issuance_authority = new List<string>() { "List-1" };
--- LeaMaPortal/Controllers/TcaController.cs
+++ LeaMaPortal/Controllers/TcaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LeaMaPortal.Models;
+using LeaMaPortal.Models.DBContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,31 @@ namespace LeaMaPortal.Controllers
 {
     public class TcaController : Controller
     {
+        private Entities db = new Entities();
         // GET: Tca
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                AgreementFormViewModel model = new AgreementFormViewModel();
+                ViewBag.TenentType = new SelectList(Common.TenentType);
+                ViewBag.TenentId = new SelectList("", "");
+                ViewBag.TenentName = new SelectList("", "");
+                ViewBag.TcaPropertyId = new SelectList("", "");
+                ViewBag.TcaPropertyIDTawtheeq = new SelectList("", "");
+                ViewBag.TcaPropertyName = new SelectList("", "");
+                ViewBag.UnitIDTawtheeq = new SelectList("", "");
+                ViewBag.UnitPropertyName = new SelectList("", "");
+
+                ViewBag.Profession = new SelectList(Common.Profession);
+                ViewBag.PropertyId = db.tbl_propertiesmaster.OrderByDescending(x => x.Property_Id).FirstOrDefault()?.Property_Id + 1;
+                //return PartialView("../Tca/Agreement/_AgreementForm");
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+             return View();
         }
 
         // GET: Tca/Details/5
@@ -23,7 +46,22 @@ namespace LeaMaPortal.Controllers
         // GET: Tca/Create
         public ActionResult Create()
         {
-            return View();
+            try
+            {
+                AgreementFormViewModel model = new AgreementFormViewModel();
+                ViewBag.TitleDisplay = new SelectList(Common.Title, Common.DefaultTitle);
+
+                ViewBag.City = new SelectList(db.tbl_region.Where(x => x.Delmark != "*").OrderBy(x => x.Region_Name), "Region_Name", "Region_Name");
+                //var country = db.tbl_country.Where(x => x.Delmark != "*").Select(x => x.Country_name);
+                ViewBag.Nationality = new SelectList(db.tbl_country.Where(x => x.Delmark != "*").OrderBy(x => x.Country_name), "Country_name", "Country_name");
+                ViewBag.Profession = new SelectList(Common.Profession);
+                ViewBag.PropertyId = db.tbl_propertiesmaster.OrderByDescending(x => x.Property_Id).FirstOrDefault()?.Property_Id + 1;
+                return PartialView("../Master/TenantIndividual/_AddOrUpdate", model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
         }
 
         // POST: Tca/Create
