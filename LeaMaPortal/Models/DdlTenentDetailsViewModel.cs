4a79ddbfd526c792c9cdb6088bbf7675b0b7cc2d
Somasundharam <somuit91@gmail.com>
2017-04-08 15:09:55 +0530

    added the class

---
+++ LeaMaPortal/Models/DdlTenentDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace LeaMaPortal.Controllers
+{
+    internal class DdlTenentDetailsViewModel
+    {
+        public SelectList TenantId { get; set; }
+        public SelectList TenantName { get; set; }
+    }
+}
