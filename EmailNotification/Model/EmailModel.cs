13202a0375b9469ac255da5b5221b60a9a9a9444
Balaganesh Balakrishnan <b.balakrishnan@lakeba.com>
2017-05-05 00:30:24 +0530

    Email Notification project added

--- EmailNotification/Model/EmailModel.cs
+++ EmailNotification/Model/EmailModel.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Notification.Model
+namespace EmailNotification.Model
 {
-    public class EmailModel
+    class EmailModel
     {
         public int Id { get; set; }
         public string From { get; set; }
         public string ToList { get; set; }
         public string CCList { get; set; }
+        public string BCCList { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
     }
---
+++ EmailNotification/Program.cs
@@ -0,0 +1,45 @@
+﻿using EmailNotification.DBContext;
+using EmailNotification.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MAIL_STATUS = "Sent";
+            try
+            {
+                LeamaEntities db = new LeamaEntities();
+                EmailHelper emailHelper = new EmailHelper();
+                var data = db.email_output.Where(w => string.IsNullOrEmpty(w.Mailstatus))
+                    .Select(s => new EmailModel
+                    {
+                        Id = s.id,
+                        ToList = s.toid,
+                        CCList = s.cc,
+                        Subject = s.Subject,
+                        Body = s.body,
+                        BCCList=s.bcc
+                    }).ToList();
+                foreach (var obj in data)
+                {
+                    emailHelper.SendMail(obj);
+                    email_output model = db.email_output.FirstOrDefault(f => f.id == obj.Id);
+                    model.Mailstatus = MAIL_STATUS;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+    }
+}
