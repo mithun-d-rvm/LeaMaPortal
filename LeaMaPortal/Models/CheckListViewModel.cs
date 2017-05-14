30b65b1e6d5ff70bfbcbf053b7701e0ac8141f06
Somasundharam <somuit91@gmail.com>
2017-05-14 10:30:32 +0530

    validation on checklist master completed

--- LeaMaPortal/Models/CheckListViewModel.cs
+++ LeaMaPortal/Models/CheckListViewModel.cs
@@ -9,12 +9,13 @@ namespace LeaMaPortal.Models
     public class CheckListViewModel
     {
         public int Id { get; set; }
-        [DisplayName("Checklist Id")]
+        [DisplayName("Checklist Id * ")]
         public string Checklist_id { get; set; }
-        [DisplayName("Checklist Name")]
+        [DisplayName("Checklist Name * ")]
         public string Checklist_Name { get; set; }
+        [DisplayName("Checklist Type * ")]
         public string Checklist_Type { get; set; }
     }
 
-    
+
 }
